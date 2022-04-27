526943c111f62f6d034c471d17d4bc6bb004cdbb
javigarcia <javigarcia@alumno-1dam>
2022-04-27 09:46:49 +0200

    genericidad

--- proJavi.07Herencia/src/_02ejemplos/_09DosTiposGenericos/TestPareja.java
+++ proJavi.07Herencia/src/_02ejemplos/_09DosTiposGenericos/TestPareja.java
@@ -1,5 +1,14 @@
 package _02ejemplos._09DosTiposGenericos;
 
+import _02ejemplos._02animales.Gato;
+import _02ejemplos._02animales.Perro;
+
 public class TestPareja {
+	public static void main(String[] args) {
+		Pareja<String, Integer> p1 = new Pareja<>("El Quijote", 450);
+		
+		Pareja<Perro, Gato> p2 = new Pareja<> (new Perro(), new Gato());
+		
+	}
 
 }
